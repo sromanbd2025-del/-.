16140fe84726e55bca8a28cc00783770d0be2643
𝐁𝐚𝐧𝐠𝐥𝐚 𝐕𝐢𝐞𝐰 𝐙𝐨𝐧𝐞 <sromanbd2025@gmail.com>
2026-04-25 10:57:15 +0600

    admin-data.admin-data.js

--- admin-data.js
+++ admin-data.js
@@ -1,6 +1,3 @@
-{
-name:"Somoy TV",
-cat:"news",
-img:"https://img.icons8.com/color/96/news.png",
-link:"https://www.youtube.com/embed/live_stream?channel=UCsM8M4b8U9L0F6n1C9rQ6lQ"
-},
+/* 📰 News */
+{name:"Somoy News",cat:"news",img:"https://img.icons8.com/color/96/news.png",
+link:"https://www.youtube.com/embed/live_stream?channel=UCsM8M4b8U9L0F6n1C9rQ6lQ"},
