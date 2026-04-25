c78272704a37785d43c000c55d35fe60b9091a61
𝐁𝐚𝐧𝐠𝐥𝐚 𝐕𝐢𝐞𝐰 𝐙𝐨𝐧𝐞 <sromanbd2025@gmail.com>
2026-04-25 10:24:15 +0600

    Add files via upload

---
+++ admin-data.js
@@ -0,0 +1,31 @@
+let channels = [
+
+{
+name:"Somoy News",
+cat:"news",
+img:"https://img.icons8.com/color/96/news.png",
+link:"https://www.youtube.com/embed/live_stream?channel=UCsM8M4b8U9L0F6n1C9rQ6lQ"
+},
+
+{
+name:"Jamuna TV",
+cat:"news",
+img:"https://img.icons8.com/color/96/news.png",
+link:"https://www.youtube.com/embed/live_stream?channel=UCN6sm8iHiPd0cnoUardDAnw"
+},
+
+{
+name:"Sports Live",
+cat:"sports",
+img:"https://img.icons8.com/color/96/football.png",
+link:"https://www.youtube.com/embed/live_stream?channel=UCpcmVJdQttn8fY2l5c5Vj1g"
+},
+
+{
+name:"Islamic TV",
+cat:"islamic",
+img:"https://img.icons8.com/color/96/mosque.png",
+link:"https://www.youtube.com/embed/live_stream?channel=UCZkN8TJ0Wz2E0v8cXh9h2hg"
+}
+
+];
