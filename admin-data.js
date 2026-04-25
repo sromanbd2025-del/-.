5f2209302ccbad428a56b23a2af2d2584404316c
𝐁𝐚𝐧𝐠𝐥𝐚 𝐕𝐢𝐞𝐰 𝐙𝐨𝐧𝐞 <sromanbd2025@gmail.com>
2026-04-25 11:00:29 +0600

    admin-data.js

--- admin-data.js
+++ admin-data.js
@@ -1,3 +1,34 @@
 /* 📰 News */
 {name:"Somoy News",cat:"news",img:"https://img.icons8.com/color/96/news.png",
 link:"https://www.youtube.com/embed/live_stream?channel=UCsM8M4b8U9L0F6n1C9rQ6lQ"},
+
+
+{name:"Jamuna TV",cat:"news",img:"https://img.icons8.com/color/96/news.png",
+link:"https://www.youtube.com/embed/live_stream?channel=UCN6sm8iHiPd0cnoUardDAnw"},
+
+{name:"Independent TV",cat:"news",img:"https://img.icons8.com/color/96/news.png",
+link:"https://www.youtube.com/embed/live_stream?channel=UC4R8DWoMoI7CAwX8_LjQHig"},
+
+{name:"Ekattor TV",cat:"news",img:"https://img.icons8.com/color/96/news.png",
+link:"https://www.youtube.com/embed/live_stream?channel=UChLtXXpo4Ge1ReTEboVvTDg"},
+
+/* ⚽ Sports */
+{name:"Sports Live 1",cat:"sports",img:"https://img.icons8.com/color/96/football.png",
+link:"https://www.youtube.com/embed/live_stream?channel=UCpcmVJdQttn8fY2l5c5Vj1g"},
+
+{name:"Sports Live 2",cat:"sports",img:"https://img.icons8.com/color/96/football.png",
+link:"https://www.youtube.com/embed/live_stream?channel=UCWJ2lWNubArHWmf3FIHbfcQ"},
+
+/* 🎬 Entertainment */
+{name:"Bangla Drama",cat:"ent",img:"https://img.icons8.com/color/96/youtube-play.png",
+link:"https://www.youtube.com/embed/live_stream?channel=UC_x5XG1OV2P6uZZ5FSM9Ttw"},
+
+{name:"Music Live",cat:"ent",img:"https://img.icons8.com/color/96/youtube-play.png",
+link:"https://www.youtube.com/embed/live_stream?channel=UC-9-kyTW8ZkZNDHQJ6FgpwQ"},
+
+/* 🕌 Islamic */
+{name:"Peace TV",cat:"islamic",img:"https://img.icons8.com/color/96/mosque.png",
+link:"https://www.youtube.com/embed/live_stream?channel=UCZkN8TJ0Wz2E0v8cXh9h2hg"},
+
+{name:"Quran Live",cat:"islamic",img:"https://img.icons8.com/color/96/mosque.png",
+link:"https://www.youtube.com/embed/live_stream?channel=UCos9fznn6s2n1iZtq6v4r2g"}
