57a7f7aca49c7d9168426ed4dbf1c1f2a0d11135
𝐁𝐚𝐧𝐠𝐥𝐚 𝐕𝐢𝐞𝐰 𝐙𝐨𝐧𝐞 <sromanbd2025@gmail.com>
2026-04-25 15:11:00 +0600

    admin-data.js

--- admin-data.js
+++ admin-data.js
@@ -1,8 +1,55 @@
 let channels = [
+
+/* 📰 NEWS */
 {
-name:"Test Channel",
+name:"Somoy TV",
 cat:"news",
-img:"https://img.icons8.com/color/96/news.png",
+img:"https://i.imgur.com/4d1YQ1b.png",
+link:"https://www.youtube.com/embed/live_stream?channel=UCsM8M4b8U9L0F6n1C9rQ6lQ"
+},
+
+{
+name:"Jamuna TV",
+cat:"news",
+img:"https://i.imgur.com/0rZ6R4K.png",
+link:"https://www.youtube.com/embed/live_stream?channel=UCN6sm8iHiPd0cnoUardDAnw"
+},
+
+{
+name:"ATN News",
+cat:"news",
+img:"https://i.imgur.com/W9b8v6R.png",
+link:"https://www.youtube.com/embed/live_stream?channel=UCmZ7gohGI9Hc9l1zWZb5Y6A"
+},
+
+{
+name:"Independent TV",
+cat:"news",
+img:"https://i.imgur.com/7yUvePI.png",
 link:"https://www.youtube.com/embed/live_stream?channel=UC4R8DWoMoI7CAwX8_LjQHig"
+},
+
+/* ⚽ SPORTS */
+{
+name:"T Sports",
+cat:"sports",
+img:"https://i.imgur.com/fv4dQ7K.png",
+link:"https://www.youtube.com/embed/live_stream?channel=UCpcmVJdQttn8fY2l5c5Vj1g"
+},
+
+{
+name:"Star Sports",
+cat:"sports",
+img:"https://i.imgur.com/zQ7xK8X.png",
+link:"https://www.youtube.com/embed/live_stream?channel=UCWJ2lWNubArHWmf3FIHbfcQ"
+},
+
+/* 🕌 ISLAMIC */
+{
+name:"Peace TV",
+cat:"islamic",
+img:"https://i.imgur.com/Kb8Zz1n.png",
+link:"https://www.youtube.com/embed/live_stream?channel=UCZkN8TJ0Wz2E0v8cXh9h2hg"
 }
+
 ];
